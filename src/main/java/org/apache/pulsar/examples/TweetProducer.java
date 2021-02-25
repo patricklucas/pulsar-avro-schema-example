498e4c3134ddd537db92db21491f77167aa42373
Patrick Lucas <me@patricklucas.com>
2021-02-25 10:51:26 +0100

    Add fixed field

--- src/main/java/org/apache/pulsar/examples/TweetProducer.java
+++ src/main/java/org/apache/pulsar/examples/TweetProducer.java
@@ -1,5 +1,11 @@
 package org.apache.pulsar.examples;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.impl.schema.AvroSchema;
@@ -23,7 +29,16 @@ public class TweetProducer {
                  .topic("twitter-avro")
                  .create()) {
                 for (int i = 0; i < 10; i++) {
-                    TwitterSchema tweet = new TwitterSchema("user-" + i, "tweet-" + i, System.currentTimeMillis());
+                    Fixed fixed = new Fixed("Hello".getBytes(StandardCharsets.UTF_8));
+                    TwitterSchema tweet = new TwitterSchema("user-" + i, "tweet-" + i, fixed, System.currentTimeMillis());
+
+                    DatumWriter<TwitterSchema> writer = new SpecificDatumWriter<>(TwitterSchema.class);
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    Encoder encoder = EncoderFactory.get().jsonEncoder(TwitterSchema.getClassSchema(), bos, true);
+                    writer.write(tweet, encoder);
+                    encoder.flush();
+                    System.out.println("Tweet JSON: " + bos.toString("UTF-8"));
+
                     producer.send(tweet);
                 }
                 System.out.println("Successfully produce 10 tweets");
